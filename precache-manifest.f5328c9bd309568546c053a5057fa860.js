2eebf1b7d0cf347f5fc0e43cece233c5778f1690
jhony maurad <jhonymaurad@gmail.com>
2020-03-13 10:04:11 -0400

    Updates

---
+++ precache-manifest.f5328c9bd309568546c053a5057fa860.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "063d0f25f9f59f39ce1030f24895e659",
+    "url": "/contact-manager/index.html"
+  },
+  {
+    "revision": "cdc8cf3e030638a5c888",
+    "url": "/contact-manager/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ada87e7fb8d98363c04c",
+    "url": "/contact-manager/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "cdc8cf3e030638a5c888",
+    "url": "/contact-manager/static/js/2.d54dc690.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/contact-manager/static/js/2.d54dc690.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ada87e7fb8d98363c04c",
+    "url": "/contact-manager/static/js/main.8e923b64.chunk.js"
+  },
+  {
+    "revision": "abc3f4757d66ed39d885",
+    "url": "/contact-manager/static/js/runtime-main.0026f48e.js"
+  }
+]);
